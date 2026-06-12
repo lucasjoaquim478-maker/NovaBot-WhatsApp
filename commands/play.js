fe05ea0b3bf984be28d5f5462efae4896eb1d701
Lucas Joaquim <lucasjoaquim478@gmail.com>
2026-06-12 17:01:00 -0300

    v6.5.10: remove -f, use yt-dlp default format

--- commands/play.js
+++ commands/play.js
@@ -35,7 +35,6 @@ async function downloadAudio(url) {
     try {
       const args = [
         ...ytDlpArgs(attempt),
-        '-f', 'ba/best',
         '--max-filesize', '25M',
         '--ffmpeg-location', FFMPEG,
         '--extract-audio',
